04da726c9dfaec28ed8a4ddbf7c93aea186b7fb4
DavidStinson <mail@davidstinson.dev>
2020-10-10 21:36:13 -0500

    type req and res

--- functions/src/api.ts
+++ functions/src/api.ts
@@ -1,3 +1,5 @@
-export const hello = function(req: any, res: any) {
+import * as express from 'express';
+
+export const hello = function(req: express.Request, res: express.Response) {
   res.send("hello, friend")
 }
